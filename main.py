ded7678180917ff6ab12ca11d876a72bc5b0981e
mahdavipanah <h.mahdavipanah@gmail.com>
2016-07-03 04:32:58 +0430

    Calculate order of tasks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 import input
+import tree
 
 # Check if input file exists
 if not os.path.isfile('input.txt'):
@@ -10,3 +11,27 @@ try:
     tasks = input.file_to_tasks('input.txt')
 except:
     sys.exit("Error happened in reading from input.txt")
+
+# Get preq tree from tasks - using tree module
+tree = tree.tree_from_tasks(tasks[:])
+
+# Ordered Tasks
+orderedTasks = []
+
+# List of available tasks
+availableTasks = [task for task in tasks if task[1] == "Start"]
+
+# While all tasks are not scheduled
+while len(orderedTasks) != len(tasks):
+    # Sort available tasks based on their required time
+    availableTasks.sort(key=lambda task: task[2])
+    # Minimum time required task
+    minTask = availableTasks[0]
+    # Add the minimum required time task to the ordered task
+    orderedTasks.append(minTask)
+    # Remove the task from available tasks
+    del availableTasks[0]
+    # Add all task childs to available task
+    for task in tree[minTask[0]]:
+        task = [t for t in tasks if t[0] == task][0]
+        availableTasks.append(task)
